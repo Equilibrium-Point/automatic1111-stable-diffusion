4ba6e1efd0a4ccbbfc52972bd09d507137626215
Maurdekye <ncolaprete@gmail.com>
2022-11-11 13:40:54 -0500

    removed extra newline

--- modules/processing.py
+++ modules/processing.py
@@ -442,7 +442,6 @@ def process_images(p: StableDiffusionProcessing) -> Processed:
 def process_images_inner(p: StableDiffusionProcessing) -> Processed:
     """this is the main loop that both txt2img and img2img use; it calls func_init once inside all the scopes and func_sample once per batch"""
 
-
     if type(p.prompt) == list:
         assert(len(p.prompt) > 0)
     else:
